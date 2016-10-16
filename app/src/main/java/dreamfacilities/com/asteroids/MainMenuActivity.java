908c1b3dc10507405bf389a888105a263cee1a54
Alejandro Catalan Sanchez <alex.catalan.sanchez@gmail.com>
2016-10-16 19:11:44 +0200

    Exercise: Add menu to Activity

--- app/src/main/java/dreamfacilities/com/asteroids/MainMenuActivity.java
+++ app/src/main/java/dreamfacilities/com/asteroids/MainMenuActivity.java
@@ -3,12 +3,14 @@ package dreamfacilities.com.asteroids;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
-    private Button bAbout;
+    private Button bAbout, bExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +24,36 @@ public class MainMenuActivity extends AppCompatActivity {
                 fireAbout(null);
             }
         });
+
+        bExit = (Button) findViewById(R.id.bExit);
+        bExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exit(null);
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { int id = item.getItemId();
+        if (id == R.id.action_settings) { return true;
+        }
+        if (id == R.id.about) {
+            fireAbout(null);
+            return true; }
+        return super.onOptionsItemSelected(item); }
 
     public void fireAbout(View view) {
         Intent i = new Intent(this, AboutActivity.class);
         startActivity(i);
+    }
+
+    public void exit(View view){
+        finish();
     }
 }
