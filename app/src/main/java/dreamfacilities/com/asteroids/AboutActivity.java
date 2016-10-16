2fa2c4668fb600edc5783f6a511ba45221edeaf0
Alejandro Catalan Sanchez <alex.catalan.sanchez@gmail.com>
2016-10-16 15:54:48 +0200

    [New] Create About dialog activity and layout

---
+++ app/src/main/java/dreamfacilities/com/asteroids/AboutActivity.java
@@ -0,0 +1,16 @@
+package dreamfacilities.com.asteroids;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by alex on 16/10/16.
+ */
+
+public class AboutActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
