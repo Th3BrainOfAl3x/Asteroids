c1034479f7cb658e5412cbad3972f069f906d56d
Alejandro Catalan Sanchez <alex.catalan.sanchez@gmail.com>
2016-10-16 16:20:00 +0200

    Exercise: Add event listener on click by code
     Please enter the commit message for your changes. Lines starting

--- app/src/main/java/dreamfacilities/com/asteroids/MainMenuActivity.java
+++ app/src/main/java/dreamfacilities/com/asteroids/MainMenuActivity.java
@@ -1,16 +1,27 @@
 package dreamfacilities.com.asteroids;
 
+import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    private Button bAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+        bAbout = (Button) findViewById(R.id.bAbout);
+        bAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fireAbout(null);
+            }
+        });
     }
 
     public void fireAbout(View view) {
